f54a1a22a3cbaf98373ca1073145ebcd0d300716
Luca Bellorio <luca.bellorio@studenti.unipd.it>
2023-05-30 17:52:34 +0200

    Aggiunte compate_intensity e compare_image

--- lib/image_helper/include/image_helper.h
+++ lib/image_helper/include/image_helper.h
@@ -13,4 +13,6 @@ int get_label(struct immagine *image);
 long int get_intensity (const struct immagine *image);
 void print (const struct immagine *image);
 long int compute_distance (const struct immagine *image1, const struct immagine *image2);
+char *compare_image (const struct immagine *image1, const struct immagine *image2);
+int compate_intensity (const struct immagine *image1, const struct immagine *image2);
 #endif
