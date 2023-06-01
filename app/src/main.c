63415b6849b8f581b51ad8ce94ae3ed3078eeb37
Stephen Pasqualotto <stephen.pasqualotto@studenti.unipd.it>
2023-06-01 18:32:00 +0200

    creato un branch diverso dal precedente 5.1 in modo da avere le stesse cartelle del branch main, sistemato il CMakelists, modificate train.c e train.h, compila però è necessaria una sessione di debug

--- app/src/main.c
+++ app/src/main.c
@@ -1,6 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "image_helper.h"
+#include "mnist.h"
+#include "train.h"
+#include "classify.h"
+
+
 int main()
 {
     // TODO MAIN CODE OF EXECUTION HERE
-
+struct immagine image;
+struct immagine *pointer;
+int label;
+image.next=train;
+pointer = image.next;
+print(&image);
+label=get_label(&image);
+printf("%d \n", label);
+print(pointer);
+label=get_label(pointer);
+printf("%d \n", label);
     return 0;
 }
--- lib/train/include/train.h
+++ lib/train/include/train.h
@@ -2,6 +2,9 @@
 #ifndef train_h
 #define train_h
 
+struct immagine *aggiungi(struct immagine *image, int index, int label);
+struct immagine train(void);
+struct immagine *rimuovi (struct immagine *ultimaimmagine);
 
 
 //TODO implement classify
