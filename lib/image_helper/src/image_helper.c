7df99af85ec9ee6150d032e6b5326798f73f0e62
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-06-01 14:32:51 +0200

    Fixed issue on image helper, was missing import of string.h

--- lib/image_helper/src/image_helper.c
+++ lib/image_helper/src/image_helper.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 #include "image_helper.h"
 #include <assert.h>
-
+#include "string.h"
 
 int pixel_at(int x, int y, const struct immagine *image)
 {
 	if (x <= 28 && y <= 28)
 	{
-		return (image->matrice[(x-1+(y-1)*28)]);
+		return (image->matrice[(x - 1 + (y - 1) * 28)]);
 	}
-		return (-1);
-}  //questa funzione funziona
+	return (-1);
+} // questa funzione funziona
 void set_label(int x, struct immagine *image)
 {
 	image->label = x;
-}  //questa funzione funziona
+} // questa funzione funziona
 int get_label(struct immagine *image)
 {
 	if (image->label > 9 || image->label < 0)
@@ -25,114 +25,109 @@ int get_label(struct immagine *image)
 	return (image->label);
 } // pronta per il testing
 
-long int get_intensity (const struct immagine *image)
+long int get_intensity(const struct immagine *image)
 {
 	long int intensit = 0;
 	int i, j;
-	for (i = 0; i <= 783; i++) 
+	for (i = 0; i <= 783; i++)
 	{
 		intensit += image->matrice[i];
 	}
 	return (intensit);
-}  // funzione pronta per il testing
+} // funzione pronta per il testing
 
-void print (const struct immagine *image)
+void print(const struct immagine *image)
 {
 	int i, j;
 	int valore;
-	for (i = 0; i <= 27; i++) 
+	for (i = 0; i <= 27; i++)
 	{
 		for (j = 0; j <= 27; j++)
 		{
-			valore = image->matrice[((i*28)+j)];
+			valore = image->matrice[((i * 28) + j)];
 			if (valore <= 64)
-				printf ("  ");
+				printf("  ");
 			if (valore <= 128 && valore > 65)
-				printf ("--");
+				printf("--");
 			if (valore <= 192 && valore > 129)
-				printf ("==");
+				printf("==");
 			if (valore >= 193)
-				printf ("XX");
-		    printf("  ");
+				printf("XX");
+			printf("  ");
 		}
 		printf("\n");
 	}
-}  // testabile
-long int compute_distance (const struct immagine *image1, const struct immagine *image2)
+} // testabile
+long int compute_distance(const struct immagine *image1, const struct immagine *image2)
 {
 	long int distanza = 0;
 	int pixel1, pixel2, differenza;
-	int i,j;
-	for (i = 0; i <= 27; i++) 
+	int i, j;
+	for (i = 0; i <= 27; i++)
 	{
 		for (j = 0; j <= 27; j++)
 		{
-			pixel1 = image1->matrice[((i*28)+j)];
-			pixel2 = image2->matrice[((i*28)+j)];	
-			differenza = pixel1-pixel2;
-			distanza = distanza +(differenza*differenza);
+			pixel1 = image1->matrice[((i * 28) + j)];
+			pixel2 = image2->matrice[((i * 28) + j)];
+			differenza = pixel1 - pixel2;
+			distanza = distanza + (differenza * differenza);
 		}
 	}
-	return(distanza);
-} //testabile
+	return (distanza);
+} // testabile
 
-int compate_intensity (const struct immagine *image1, const struct immagine *image2){
+int compate_intensity(const struct immagine *image1, const struct immagine *image2)
+{
 	long int x, y;
 
-	x = get_intensity (image1);
-	y = get_intensity (image2);
+	x = get_intensity(image1);
+	y = get_intensity(image2);
 
-	assert ((x != 0) & (y != 0));
+	assert((x != 0) & (y != 0));
 
 	if (x > y)
 		return 1;
-	else 
+	else
 		return 2;
 }
 
-char *compare_image (const struct immagine *image1, const struct immagine *image2){
+char *compare_image(const struct immagine *image1, const struct immagine *image2)
+{
 	int pixel1, pixel2;
-	int i,j,y;
-    char vero [5] = "vero";
-    char falso [6] = "falso";
-	
-	char *risultato = malloc (sizeof(char) * 6);
-    if (risultato == NULL){
-		printf ("Errore nella malloc \n");
-		exit (1);
+	int i, j, y;
+	char vero[5] = "vero";
+	char falso[6] = "falso";
+
+	char *risultato = malloc(sizeof(char) * 6);
+	if (risultato == NULL)
+	{
+		printf("Errore nella malloc \n");
+		exit(1);
 	}
-	
-	for (i = 0; i <= 27; i++) 
+
+	for (i = 0; i <= 27; i++)
 	{
 		for (j = 0; j <= 27; j++)
 		{
-			pixel1 = image1->matrice[((i*28)+j)];
-			pixel2 = image2->matrice[((i*28)+j)];		
+			pixel1 = image1->matrice[((i * 28) + j)];
+			pixel2 = image2->matrice[((i * 28) + j)];
 
-            if (pixel1 == pixel2){
-                strcpy (risultato, vero);
+			if (pixel1 == pixel2)
+			{
+				strcpy(risultato, vero);
 				y = 0;
-			}	
-            else {
-                strcpy (risultato, falso);
+			}
+			else
+			{
+				strcpy(risultato, falso);
 				y = 1;
-            }        	
-      	}
-		if ( y == 1)
-		break; 
-    }
+			}
+		}
+		if (y == 1)
+			break;
+	}
 	return risultato;
-} //Quando la si usa ricordare di fare il free
-
-
-
-
-
-
-
-
-
-
+} // Quando la si usa ricordare di fare il free
 
 /*
 int main()
@@ -147,17 +142,17 @@ int main()
 	valore = get_label (&picture);
 	printf ("%d \n", valore);
 	int i,j;
-	for (i = 0; i <= 27; i++) 
-	{ 
-        //k += 10;
+	for (i = 0; i <= 27; i++)
+	{
+		//k += 10;
 		for (j = 0; j <= 27; j++)
 		{
 			picture.matrice[i][j] = 3;
 		}
 	}
-	for (i = 0; i <= 27; i++) 
-	{ 
-        //k += 10;
+	for (i = 0; i <= 27; i++)
+	{
+		//k += 10;
 		for (j = 0; j <= 27; j++)
 		{
 			picture2.matrice[i][j] = 2;
@@ -165,7 +160,7 @@ int main()
 	}
 	long int intensit = get_intensity(&picture);
 	printf ("%ld \n", intensit);
-    print(&picture);
+	print(&picture);
 	long int distanza = compute_distance(&picture, &picture2);
 	printf ("%ld \n", distanza);
 
