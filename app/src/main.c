8c2f7e2808630897216ed029b4ff0d57fdf04faf
Leonardo Parise <leonardo.parise.1@studenti.unipd.it>
2023-06-04 10:11:51 +0200

    aggiornata funzione classify

--- app/src/main.c
+++ app/src/main.c
@@ -22,30 +22,33 @@ int main(void)
  int tlabels[10000];
  double tmatrix[784];
   struct immagine *list;  
+  struct immagine immagine_simile;
  
  list=train(head);
 
  
 
- load_mnist();  //OK
- label_char2int(10000, test_label_char, tlabels); //OK
- printf("\n%d\n",tlabels[9]);
- set_label(tlabels[9], &imagetest);
+ load_mnist();  
+ label_char2int(10000, test_label_char, tlabels); 
+ printf("\n%d\n",tlabels[5000]);
+ set_label(tlabels[5000], &imagetest);
 
  
-image_char2double(9, test_image_char, tmatrix); 
+image_char2double(5000, test_image_char, tmatrix); 
  for(int y = 0; y < 28; y++){
 		for(int j = 0; j < 28; j++){
-	    imagetest.matrice[(y*28+j)] = tmatrix[(y*28+j)]; //OK
+	    imagetest.matrice[(y*28+j)] = tmatrix[(y*28+j)]; 
 			}}
 		
 
-imagetest.intensity = get_intensity(&imagetest);  //OK
+imagetest.intensity = get_intensity(&imagetest);  
 
-printf("\nQuesto è il label che vorrei ottenere: %d\n",imagetest.label);//OK
+print(&imagetest);
+printf("\nQuesto è il label che vorrei ottenere: %d\n",imagetest.label);
 
 
-matching_number=classify(&imagetest,list); 
+matching_number=classify(&imagetest,list,&immagine_simile); 
+print(&immagine_simile);
 printf("\nquesto è il label dell'immagine simile %d\n",matching_number); 
 
 return 0;
--- lib/classify/src/classify.c
+++ lib/classify/src/classify.c
@@ -4,10 +4,11 @@
 #include "mnist.h"
 #include "image_helper.h"
 
-int classify(const struct immagine *image, struct immagine *list_images){
+int classify(const struct immagine *image, struct immagine *list_images, struct immagine *corresponding_image){
 	 
   long int mindistance ; //calcolo la distanza tra un'immagine della lista e quella da analizzare
-  int corresponding_label; //creo una variabile che conterrà il label dell'immagine fornita in input
+  int corresponding_label;
+//creo una variabile che conterrà il label dell'immagine fornita in input
   
    mindistance = compute_distance(image,list_images);  //!PROBLEMA CON LIST_IMAGES
   while( list_images!=NULL) //ricerca iterativa
@@ -18,7 +19,9 @@ int classify(const struct immagine *image, struct immagine *list_images){
   if(distance<mindistance)  //caso in cui trovo un'immagine più affine a quella da analizzare
   {
    mindistance=distance;      //registro la nuova distanza minima
-   corresponding_label=list_images->label;}//salvo l'indirizzo
+   corresponding_label=list_images->label;
+      *(corresponding_image) = *(list_images);}//salvo l'indirizzo
+ 
     list_images=list_images->next;
   }
   
