c724e2667e01ef7c658a1bf4c79339273805d6ea
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-06-05 14:04:04 +0200

    Fixed comment style.

--- lib/image_helper/include/image_helper.h
+++ lib/image_helper/include/image_helper.h
@@ -2,6 +2,14 @@
 #ifndef image_helper_h
 #define image_helper_h
 
+/*
+ * This is the struct of image:
+ *
+ * matrice: [double[]] is the matrix of the image
+ * label: [int] is the label of the image
+ * intensity: [long int] is the intensity of the image
+ * next: [struct immagine] is the pointer to the next image
+ */
 struct immagine
 {
 	double matrice[784];
@@ -11,44 +19,43 @@ struct immagine
 };
 
 /*
-	This function return the value of the pixel at the position (x,y) of the image.
-*/
+ * This function return the value of the pixel at the position (x,y) of the image.
+ */
 double pixel_at(int x, int y, const struct immagine *image);
 
-
 /*
-	This function set the value of the label with the value x.
-*/
+ * This function set the value of the label with the value x.
+ */
 void set_label(int x, struct immagine *image);
 
 /*
-	This function return the value of the label of the image.
-*/
+ * This function return the value of the label of the image.
+ */
 int get_label(struct immagine *image);
 
 /*
-	This function return the value of the intensity of the image.
-*/
+ * This function return the value of the intensity of the image.
+ */
 long int get_intensity(const struct immagine *image);
 
 /*
-	This function print the image.
-*/
+ * This function print the image.
+ */
 void print(const struct immagine *image);
 
 /*
-	This function return the distance between two images.
-*/
+ * This function return the distance between two images.
+ */
 long int compute_distance(const struct immagine *image1, const struct immagine *image2);
 
 /*
-	This function return the image with the minimum distance from the image1.
-*/
+ * This function return the image with the minimum distance from the image1.
+ */
 char *compare_image(const struct immagine *image1, const struct immagine *image2);
 
 /*
-	This function return 1 if the intensity of the image 1 is bigger than intensity of image2.
-*/
+ * This function return 1 if the intensity of the image 1 is bigger than intensity of image2.
+ */
 int compate_intensity(const struct immagine *image1, const struct immagine *image2);
 
 #endif
