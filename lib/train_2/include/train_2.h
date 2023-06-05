9f359386dd619b900cb19c5bea03deb54d629812
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-06-05 14:01:33 +0200

    Added some specifications to the functions.

--- lib/train_2/include/train_2.h
+++ lib/train_2/include/train_2.h
@@ -1,9 +1,19 @@
 #ifndef train_2_h
 #define train_2_h
 
-struct immagine *train_ordinato (struct immagine *head);
-void ordina_lista (struct immagine *immagine);
-void rimuovi(struct immagine *ultimaimmagine);
+/*
+ *  Struttura dati per la lista di immagini
+ */
+struct immagine *train_ordinato(struct immagine *head);
 
+/*
+ *  Funzioni per ordinare la lista di immagini
+ */
+void ordina_lista(struct immagine *immagine);
+
+/*
+ *  Funzioni per rimuovere l'ultima immagine
+ */
+void rimuovi(struct immagine *ultimaimmagine);
 
 #endif
