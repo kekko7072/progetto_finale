eaab0c0ee0ec895e9a4b5ddb8ce383d51d39e72a
Luca Bellorio <luca.bellorio@studenti.unipd.it>
2023-06-05 13:19:39 +0200

    Modifica per verificare funzionamento train_2

--- app/src/main.c
+++ app/src/main.c
@@ -1,6 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "image_helper.h"
+#include "mnist.h"
+#include "train_2.h"
+#include "string.h"
+
+
 int main()
 {
-    // TODO MAIN CODE OF EXECUTION HERE
+    printf ("Ciao\n");
+    struct immagine *head = NULL;
+    struct immagine *testa = NULL;
+    /*int label;
+    double pixel;
+    */
+    head = train_ordinato(head);
+    /*pixel = pixel_at(10, 10, head);
+    printf("pixel: %f \n", pixel);
+    printf("intensit: %ld \n", head->intensity);
+    label = get_label(head);
+    printf("label:%d \n", label);
+    print(head);
+    for (int j = 0; j < 59999; j++)
+        head = head->next;
+    pixel = pixel_at(10, 10, head);
+    printf("pixel: %f \n", pixel);
+    printf("intensit: %ld \n", head->intensity);
+    label = get_label(head);
+    printf("label:%d \n", label);
+    print(head);
+    */
+
+   for (int i = 0; i < 30; i++){
+    for (testa = head; testa != NULL; testa = testa->next){
+        printf ("%ld", testa->intensity);
+        printf ("\n");
+    }
+   }
+
+    rimuovi(testa);
 
     return 0;
 }
