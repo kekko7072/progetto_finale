49bc3b7bbaf13400f43a98fc16647c5dc799d99f
Leonardo Parise <leonardo.parise.1@studenti.unipd.it>
2023-06-06 22:08:12 +0200

    code cleaning

--- lib/classify/src/classify.c
+++ lib/classify/src/classify.c
@@ -31,40 +31,49 @@ int classify(const struct immagine *image, struct immagine *list_images, struct 
   return corresponding_label; // ritorno l'immagine della lista più vicina
 }
 
-void testchoice(struct immagine *testimage)
-{
 
-  int numbertest = -1;
-  int tlabels[10000];
-  double tmatrix[784];
-  char string_numbertest[10000];
 
-  while (numbertest > 9999 || numbertest < 0)
-  {
-    printf("\ndigit a number from 0 to 9999: ");
-    scanf("%s", string_numbertest);
-    numbertest = atoi(string_numbertest);
+void testchoice(struct immagine *testimage){
 
-    if (numbertest > 9999 || numbertest < 0)
-    {
-      printf("\nthe chosen number is outside the asked range\n");
-    }
+int numbertest=-1;
+ int tlabels[10000];
+ double tmatrix[784];
+ char string_numbertest[100]; 
+ int NUM=1;
+
+
+while(numbertest>9999 || numbertest<0)
+{ 
+  printf("\nDigitare un numero da 0 a 9999: ");
+	scanf("%s", string_numbertest);
+  
+  
+ numbertest = atoi(string_numbertest);
+    for(int i=0;i<(int)strlen(string_numbertest);i++)
+  {if(string_numbertest[i]>'9'||string_numbertest[i]<'0')
+   {NUM=0;
+   numbertest=-1;}
+   
   }
+ 
+ if(NUM)
+ {if(numbertest>9999 || numbertest<0)
+ {printf("\nIl numero scelto è fuori dall'intervallo richiesto\n");}}
+else
+{printf("\nInserire solo numeri, perfavore\n");
+NUM=1;}
+ }
 
-  load_mnist();
-  label_char2int(10000, test_label_char, tlabels);
-  printf("\n%d\n", tlabels[numbertest]);
+load_mnist();  
+label_char2int(10000, test_label_char, tlabels); 
+set_label(tlabels[numbertest], testimage);
 
-  set_label(tlabels[numbertest], testimage);
-
-  image_char2double(numbertest, test_image_char, tmatrix);
-  for (int y = 0; y < 28; y++)
-  {
-    for (int j = 0; j < 28; j++)
-    {
-      testimage->matrice[(y * 28 + j)] = tmatrix[(y * 28 + j)];
-    }
-  }
-
-  testimage->intensity = get_intensity(testimage);
+ 
+image_char2double(numbertest, test_image_char, tmatrix); 
+for(int y = 0; y < 28; y++){
+	for(int j = 0; j < 28; j++){
+	    testimage->matrice[(y*28+j)] = tmatrix[(y*28+j)]; 
+			}}
+		
+testimage->intensity = get_intensity(testimage);  
 }
