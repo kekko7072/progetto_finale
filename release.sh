e4fbcc141fd04ac0e9af4dbc52b93edbe315575d
Francesco Vezzani <32552589+kekko7072@users.noreply.github.com>
2023-06-06 11:21:22 +0200

    Added fixes to release.sh

--- release.sh
+++ release.sh
@@ -4,9 +4,13 @@
 
 # Prompt the user to choose a test
 echo "Please choose a release to build:"
-echo "1. 5.1"
-echo "2. 5.2"
-echo "3. Exit"
+echo "0. 5.1"
+echo "1. 5.1 - Optimized 1"
+echo "2. 5.1 - Optimized 2"
+echo "3. 5.2"
+echo "4. 5.2 - Optimized 1"
+echo "5. 5.2 - Optimized 1"
+echo "6. Exit"
 
 # Read user input
 read -p "Enter your choice (1-3): " choice
@@ -15,21 +19,35 @@ read -p "Enter your choice (1-3): " choice
 rm -rf build && mkdir build
 
 # Enter in build and run cmake
-cd build && cmake .. && 
+cd build && cmake ..
 
 # Execute the corresponding test based on the user's choice
 case $choice in
-    1)
+    0)
         echo "Running 5.1 build..."
         ;;
+    1)
+        echo "Running 5.1 build optimized 1..."
+        gcc -Wall -O1 
+        ;;
     2)
-        echo "Running 5.2 build..."
+        echo "Running 5.1 build optimized 2..."
+        gcc -Wall -O1 
         ;;
     3)
+        echo "Running 5.2 build..."
+        ;;
+    4)
+        echo "Running 5.2 build optimized 1..."
+        ;;
+    5)
+        echo "Running 5.2 build optimized 2..."
+        ;;
+    6)
         echo "Exiting..."
         exit 0
         ;;
     *)
-        echo "Invalid choice. Please enter a number between 1 and 3."
+        echo "Invalid choice. Please enter a number between 0 and 6."
         ;;
 esac
