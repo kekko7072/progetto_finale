6afc16f03857c1afdf5550ebc0f50a0bb832a017
Francesco Vezzani <francesco.vezz01@gmail.com>
2023-06-16 10:24:42 +0200

    Improvements on stability for release.sh

--- release.sh
+++ release.sh
@@ -6,9 +6,7 @@
 echo "Please choose a release to build:"
 echo "1. 5.1"
 echo "2. 5.2"
-echo "3. 5.1 - Optimized 1"
-echo "4. 5.2 - Optimized 1"
-echo "5. Exit"
+echo "3. Exit"
 
 # Read user input
 read -p "Enter your choice (1-5): " choice
@@ -34,35 +32,7 @@ case $choice in
         chmod +x ./app_2 
         ./app_2
         ;;
-    3)
-        cd .. && mkdir release
-        echo "Compiling 5.1 optimized 1..."
-        gcc -Wall -O1 -o release/app_5_1_opt_1 app/src/main.c \
-            -Ilib/image_helper/include \
-            -Ilib/classify/include \
-            -Ilib/mnist/include \
-            -Ilib/train/include \
-            lib/image_helper/src/image_helper.c \
-            lib/mnist/src/mnist.c \
-            lib/classify/src/classify.c \
-            lib/train/src/train.c 
-        echo "This code has been compiled but data sets are not reachable..."
-        ;;
-    4)
-        cd .. && mkdir release
-        echo "Compiling 5.1 optimized 2..."
-        gcc -Wall -O2 release/app_5_2_opt_2 app/src/main_2.c \
-            -Ilib/image_helper/include \
-            -Ilib/classify/include \
-            -Ilib/mnist/include \
-            -Ilib/train/include \
-            lib/image_helper/src/image_helper.c \
-            lib/mnist/src/mnist.c \
-            lib/classify2/src/classify2.c \
-            lib/train2/src/train2.c 
-        echo "This code has been compiled but data sets are not reachable..."
-        ;;
-    5)
+    3)  
         echo "Exiting..."
         exit 0
         ;;
